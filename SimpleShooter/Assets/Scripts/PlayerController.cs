9c382c01c2356f1ec42546b87e3059bbbdedadf8
mohithsrao <mohithsrao@gmail.com>
2018-02-14 22:15:57 +0530

    Added Player and Movement

---
+++ SimpleShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigiBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigiBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        myRigiBody.MovePosition(myRigiBody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 moveVelocity)
+    {
+        velocity = moveVelocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 elevatedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(elevatedPoint);
+    }
+}
